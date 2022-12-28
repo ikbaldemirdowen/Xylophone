b8741c0687641b573e8c8592a440efe033d9dc99
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2022-12-27 22:15:52 -0800

    alpha change for button

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -17,12 +17,13 @@ class ViewController: UIViewController {
 
     @IBAction func keyPressed(_ sender: UIButton)
     {
+        //first alpha
         sender.alpha = 0.5
         backgroundColor = sender.backgroundColor
         mainView.backgroundColor = backgroundColor
         playSound(sender.currentTitle!)
         DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(Int(Double(0.2))), execute: {
-            sender.alpha = 1
+            sender.alpha = 1.0
         })
     }
     
